91080e6d632e6b500291950fcc8e583a4f89bfd3
CollenK <collen.kriel@gmail.com>
2016-08-03 14:12:13 +0700

    5.8 Added comments controller, Raty ratings plugin and styled comments

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,12 +2,8 @@ class Product < ActiveRecord::Base
   has_many :orders
   has_many :comments
 
-  def highest_rating_comment
-    comments.rating_desc.first
-  end
-
-  def lowest_rating_comment
-    comments.rating_asc.first
+  def average_rating
+    comments.average(:rating).to_f
   end
 
 end
