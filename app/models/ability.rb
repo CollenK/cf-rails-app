fa54a0ffe420490b760eac41d5fd92b92a40daa2
CollenK <collen.kriel@gmail.com>
2016-08-06 15:38:14 +0700

    Added admin falg to users, restrict destroy action for comments to admin only

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,6 +2,13 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    user ||= User.new # guest user (not logged in)
+    if user.admin?
+      can :manage, :all
+    else
+      can :read, :all
+    end
+
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
@@ -28,7 +35,5 @@ class Ability
     #
     # See the wiki for details:
     # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
-    user ||= User.new # guest user (not logged in)
-    can :manage, User, id: user.id
   end
 end
