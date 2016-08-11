bb0df57d589cdbfa70ef9f00e9b7dd1f9e7d865f
CollenK <collen.kriel@gmail.com>
2016-08-11 13:18:28 +0700

    6.2 Added specs for product/comment model

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe Product do
+  context "when the product has comments" do
+    before do
+      @product = Product.create!(name: "camera")
+      @user = User.create!(email: "test@example.com", password: "password")
+      @product.comments.create!(rating: 1, user: @user, body: "Bad")
+      @product.comments.create!(rating: 3, user: @user, body: "Average")
+      @product.comments.create!(rating: 5, user: @user, body: "Great")
+    end
+
+    it "returns the average raiting of all comments" do
+      expect(@product.average_rating).to eq 3
+    end
+  end
+
+  context "when the product has no name" do
+    before do
+      @product = Product.new(description: "Great camera!")
+    end
+
+    it "is an invalid product" do
+      expect(@product).not_to be_valid
+    end
+  end
+end
