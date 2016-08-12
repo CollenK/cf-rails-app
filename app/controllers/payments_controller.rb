ad76fe6be1b1ff64d16f25fc42ec320d0b1e5265
CollenK <collen.kriel@gmail.com>
2016-08-12 12:54:35 +0700

    6.6 Created Payments controller, Orders, and order confirmation page

---
+++ app/controllers/payments_controller.rb
@@ -0,0 +1,31 @@
+class PaymentsController < ApplicationController
+  def create
+	  token = params[:stripeToken]
+	  @product = Product.find(params[:product_id])
+	  @user = current_user
+    # Create the charge on Stripe's servers - this will charge the user's card
+  	begin
+  		charge = Stripe::Charge.create(
+  			:amount => (@product.price*100).to_i,
+  			:currency => "usd",
+  			:source => token,
+  			:description => params[:stripeEmail]
+  		)
+
+		if charge.paid
+			Order.create!(product_id: @product.id, user_id: @user.id, total: @product.price)
+		end
+
+		flash[:success] = "Payment processed successfully"
+
+  	rescue Stripe::CardError => e
+      # The card has been declined
+  		body = e.json_body
+  		err  = body[:error]
+  		flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
+  	end
+
+  	redirect_to static_pages_order_confirmation_path
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,9 @@ Rails.application.routes.draw do
   resources :orders, only: [:index, :show, :create, :destroy]
   get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
+  get 'static_pages/order_confirmation'
   post 'static_pages/thank_you'
+  post 'payments/create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
